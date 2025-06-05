41aa4477032c9fc5c1cdf1e844529e956e19bc8b
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-06-05 18:49:17 -0500

    Tercer cambio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      url: process.env.DATABASE_URL,
       port: 5432,
       username: 'postgres', // tu usuario de postgres
-      password: '090306',    // tu contraseña
-      database: 'notification_db',
-      autoLoadEntities: true,
+      password: 'wlLVyctJCSQadFPjDgTypMkccRCdQSGR',    // tu contraseña
+      database: 'railway',
+      entities: [__dirname + `/**/*.entity{.ts,.js}`],
       synchronize: true,
     }),
- 
-  ],
+  NotificationsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
