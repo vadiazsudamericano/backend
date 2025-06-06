8d4da932e01a4fe2f1366127d2519f33800adbe2
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-06-05 19:08:06 -0500

    Cuarto cambio

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,19 @@ import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
+  imports: [
+     TypeOrmModule.forRoot({
+     type: 'postgres',
       url: process.env.DATABASE_URL,
-      port: 5432,
-      username: 'postgres', // tu usuario de postgres
-      password: 'wlLVyctJCSQadFPjDgTypMkccRCdQSGR',    // tu contraseña
-      database: 'railway',
-      entities: [__dirname + `/**/*.entity{.ts,.js}`],
-      synchronize: true,
-    }),
+      port: 5432,
+      username: 'postgres', // tu usuario de postgres
+      password: 'wlLVyctJCSQadFPjDgTypMkccRCdQSGR', // tu contraseña
+      database: 'railway',
+      entities: [__dirname + `/**/*.entity{.ts,.js}`],
+      synchronize: true,
+}),
   NotificationsModule],
-  controllers: [AppController],
-  providers: [AppService],
+ controllers: [AppController],
+ providers: [AppService],
 })
 export class AppModule {}
